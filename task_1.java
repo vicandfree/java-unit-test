f63ef24727b2a24053004b2131593733f8d1a05e
vicandfree <vicandfree@gmail.com>
2023-08-27 22:47:02 +0300

    My unit tests

---
+++ task_1.java
@@ -0,0 +1,7 @@
+@Test
+public void checkIsAdultWhenAgeIsMoreThan18True() {
+    Program program = new Program();
+    boolean isAdult = program.checkIsAdult(19);
+    boolean expected = true;
+	assertEquals("Что-то пошло не так...", expected, isAdult);
+}
